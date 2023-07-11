4a3277efca596e2c6a05f5ad57e46c2a118b56cf
Aqil Anwar <aqilanwarx@gmail.com>
2023-07-12 07:22:19 +0800

    Create seeder to store data from CSV to database.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Test;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +19,33 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $file = storage_path('app/Table_Input.csv'); 
+
+        $handle = fopen($file, 'r');
+        fgetcsv($handle);
+    
+                
+        $result = [] ; 
+        if ($handle !== false) {
+            
+            while (($data = fgetcsv($handle)) !== false) {
+                $index = $data[0];  
+                $value = $data[1]; 
+    
+                $result[] = (object) [
+                    'index' => $index , 
+                    'value' => $value
+                ];
+                
+            }
+        }
+        //Save the detail from csv to database
+        foreach($result as $res){
+            $test = new Test;
+            $test->index = $res->index;
+            $test->value = $res->value;
+            $test->save();
+        }
     }
 }
